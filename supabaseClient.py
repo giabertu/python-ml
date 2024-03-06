7cb82421c0e372635cd8b7c735326763cc0f000c
giabertu <giabertu99@gmail.com>
2024-03-06 11:39:10 +0000

    feat: model setup

--- supabaseClient.py
+++ supabaseClient.py
@@ -1,7 +1,7 @@
 import os
 from supabase import create_client, Client
 
-url: str = os.environ.get('https://drxkhmuiheyogtzbktoq.supabase.co')
-key: str = os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyeGtobXVpaGV5b2d0emJrdG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5NjU2NzUsImV4cCI6MjAyNDU0MTY3NX0.nybBNviS2Oy0mUcXwco8YX8yINFwMxVDRRPT9HGgShg')
+url: str = 'https://drxkhmuiheyogtzbktoq.supabase.co'
+key: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyeGtobXVpaGV5b2d0emJrdG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5NjU2NzUsImV4cCI6MjAyNDU0MTY3NX0.nybBNviS2Oy0mUcXwco8YX8yINFwMxVDRRPT9HGgShg'
 
 supabase: Client = create_client(url, key)
